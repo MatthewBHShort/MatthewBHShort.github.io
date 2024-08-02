d7a9acea7b60982a81abe4da2dedae6fa2f61773
Matthew Short <matthewshort@Matthews-MacBook-Air.local>
2024-08-02 09:54:47 -0600

    Adding data points

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ function saveString(passedThroughString) {
     console.log(passedThroughString);
     const inputString = passedThroughString;
     localStorage.setItem('sharedString', inputString);
-    window.location.href = 'https://matthewbhshort.github.io/results.html';
+    // window.location.href = 'https://matthewbhshort.github.io/results.html';
     
 
 }
@@ -332,6 +332,7 @@ function saveData(stringResponses,drivers){
     
     if(drivers.includes("costs")){
         addToTally('costs',user);
+        console.log("Saving new Survey Responses");
         saveSurveyResponse(surveyResponse);
     }
     if(drivers.includes("ghgs")){
